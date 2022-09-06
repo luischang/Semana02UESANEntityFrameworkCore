ec9322ffdf0a3aee1435c934a3e40581f1cedcbd
Luis Chang <luis.chang@outlook.com>
2022-09-06 08:20:15 -0500

    Agregar archivos de proyecto.

---
+++ Semana02UESANEntityFrameworkCore.CodeFirst/Models/MundialQatar2022DbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana02UESANEntityFrameworkCore.CodeFirst.Models
+{
+    public class MundialQatar2022DbContext: DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var cnx = "";
+            optionsBuilder
+                .UseSqlServer(cnx)
+                .LogTo(Console.WriteLine, LogLevel.Information);        
+        }
+
+        public virtual DbSet<Player> Player { get; set; }
+
+
+    }
+}
---
+++ Semana02UESANEntityFrameworkCore.CodeFirst/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana02UESANEntityFrameworkCore.CodeFirst.Models
+{
+    [Table("Player")]
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string FullName { get; set; }
+        [Column(TypeName = "int")]
+        public int Dorsal { get; set; }
+    }
+}
